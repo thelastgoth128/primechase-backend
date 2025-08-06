ee9fe7d6568f9ede4a57295ba109a061385a5b49
thelastgoth128 <thindwacyrus86@gmail.com>
2025-08-06 12:06:57 +0200

    create auth resource with crud operations

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './components/user/user.module';
+import { AuthModule } from './components/auth/auth.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
