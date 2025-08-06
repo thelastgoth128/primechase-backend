bffc3339af2196c37181049d11f691d000d30f0d
thelastgoth128 <thindwacyrus86@gmail.com>
2025-08-06 13:33:33 +0200

    setup database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './components/user/user.module';
 import { AuthModule } from './components/auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './components/user/entities/user.entity';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:".env"
+    }),
+    TypeOrmModule.forRootAsync({
+      imports:[ConfigModule],
+      useFactory:async (configService : ConfigService)=>({
+        type: "postgres",
+        url:configService.get<string>('DATABASE_URL'),
+        entities:[User],
+        synchronize:false,
+      }),
+      inject: [ConfigService],
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
