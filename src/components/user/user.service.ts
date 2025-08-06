76d3cb1767823b4ff602df10c53077002552c583
thelastgoth128 <thindwacyrus86@gmail.com>
2025-08-06 18:05:00 +0200

    create user method

--- src/components/user/user.service.ts
+++ src/components/user/user.service.ts
@@ -1,11 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { JwtService } from '@nestjs/jwt';
+import { Role } from '../enums/role.enum';
+import { access } from 'fs';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private readonly userrep: Repository<User>,
+    private jwtService : JwtService
+  ){}
+
+  async create(createUserDto: CreateUserDto) {
+    const { email } = createUserDto
+
+    const exists = await this.userrep.find({where: {email}})
+    if (exists) {
+      throw new ForbiddenException('email already exists, please login')
+    }
+    createUserDto.role = Role.CUSTOMER
+    const user = await this.userrep.save(createUserDto)
+
+    const payload = {
+      userid: user.userid,
+      name: user.name,  
+      email: user.email,
+      role: user.role,
+      createdat: user.created_at
+    }
+    const jwt = await this.jwtService.signAsync(payload, {
+      secret:process.env.JWT_SECRET
+    })
+    return {
+      access_token: jwt,
+      data: payload
+    }
   }
 
   findAll() {
