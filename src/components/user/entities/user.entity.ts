dfd6b5eb1d92cbe237d67642b14d3b1b20f81b6f
thelastgoth128 <thindwacyrus86@gmail.com>
2025-08-06 13:02:00 +0200

    create a user entity

--- src/components/user/entities/user.entity.ts
+++ src/components/user/entities/user.entity.ts
@@ -1,1 +1,27 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    userid : number
+
+    @Column()
+    name : string
+
+    @Column()
+    email : string
+
+    @Column()
+    password : string
+
+    @Column({
+        type: 'enum',
+        enum: Role,
+        default:Role.customer
+    })
+    role : Role
+
+    @Column()
+    created_at : Date
+}
