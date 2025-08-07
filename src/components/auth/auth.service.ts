db8dac36cfd773d1f4df16b5a7d730815cd809ec
thelastgoth128 <thindwacyrus86@gmail.com>
2025-08-07 10:56:59 +0200

    implement forgotpassword and verifytoken methods

--- src/components/auth/auth.service.ts
+++ src/components/auth/auth.service.ts
@@ -8,6 +8,9 @@ import { UserService } from '../user/user.service';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt'
 import { Response } from 'express';
+import { MailService } from '../services/mail.service';
+import { nanoid } from "nanoid";
+import { addMinutes } from 'date-fns'
 
 @Injectable()
 export class AuthService {
@@ -16,6 +19,7 @@ export class AuthService {
     private readonly userrep : Repository<User>,
     private readonly userService : UserService,
     private readonly jwtService : JwtService,
+    private readonly mailService : MailService
   ){}
  
   async signIn(email: string, pass: string){
@@ -38,6 +42,51 @@ export class AuthService {
       access_token : jwt,
       data : payload
     }
+  }
+
+  async forgotPassword(email:string){
+    const user = await this.userService.findMail(email)
+
+    if(user){
+      const reset_token = nanoid(64)
+      const expirationTime = addMinutes(new Date(), 60)
+
+      await this.userService.saveResetToken(email, reset_token, expirationTime)
+      await this.mailService.sendPasswordResetEmail(email, reset_token)
+    }
+  }
+
+  async verifyResetToken(email:string, token:string){
+    const user = await this.userService.findMail(email)
+    if(!user || user.reset_token || !user.reset_token_expiry){
+      throw new Error('Invalid or expired token')
+    }
+
+    const noew = new Date()
+    if( noew > new Date(user.reset_token_expiry) ){
+      throw new Error('Token has expired')
+    }
+    if ( user.reset_token !== token){
+      throw new Error ('Invalid token')
+    }
+    return user
+  }
+
+  async resetPassword(user: User, newPassword:string){
+    user.password = await this.hashPassword(newPassword)
+    user.reset_token = null
+    user.reset_token_expiry = null
+
+    await this.userrep.save(user)
+
+    return {
+      message: 'Password has been successfully reset'
+    }
+  }
+
+  private async hashPassword(password:string){
+    const hashPassword = await bcrypt.hash(password,12)
+    return hashPassword
   }
 
   async logout(res:Response){
