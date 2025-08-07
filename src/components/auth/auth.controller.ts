cdd91d53f7106ab7947020602b631f37a653b245
thelastgoth128 <thindwacyrus86@gmail.com>
2025-08-07 11:53:47 +0200

    implement auth controller

--- src/components/auth/auth.controller.ts
+++ src/components/auth/auth.controller.ts
@@ -1,34 +1,38 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { sign } from 'crypto';
+import type { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService
+  ) {}
+
+  @Post('login')
+  async signIn(@Body() signInDto:Record<string,any>){
+    return this.authService.signIn(signInDto.email, signInDto.password);
+  }
+
+  @Post('forgot-password')
+  async forgotpassword(@Body() forgotPasswordDto:Record<string,any>){
+    return await this.authService.forgotPassword(forgotPasswordDto.email)
+  }
 
   @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
+  async resetPassword(
+      @Body('email') email: string,
+      @Body('token') token : string,
+      @Body('newPassword') newPassword: string
+  ){
+      const user = await this.authService.verifyResetToken(email,token)
+      return this.authService.resetPassword(user,newPassword)
   }
 
-  @Get()
-  findAll() {
-    return this.authService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
-  }
+    @Post('logout')
+    async logout(@Res() response: Response){
+      return this.authService.logout(response)
+    }
 }
