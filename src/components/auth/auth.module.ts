b2ad1147827c134f8bd3223120be349627ee7923
thelastgoth128 <thindwacyrus86@gmail.com>
2025-08-08 10:40:09 +0200

    setup auth module with authguard

--- src/components/auth/auth.module.ts
+++ src/components/auth/auth.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UserModule } from '../user/user.module';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './guards/authGuard';
+import { MailService } from '../services/mail.service';
 
 @Module({
+  imports: [
+    UserModule,
+    JwtModule.register({
+      global:true,
+      secret:process.env.SECRET,
+      signOptions: {expiresIn:'1d'}
+    })
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService,{
+    provide: APP_GUARD,
+    useClass: AuthGuard,
+  },MailService,JwtService],
+  exports: [JwtService]
 })
 export class AuthModule {}
