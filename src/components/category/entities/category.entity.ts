f2598588ba6583198eb5d8799487f18232e8058d
thelastgoth128 <thindwacyrus86@gmail.com>
2025-08-09 08:34:36 +0200

    add a description column

--- src/components/category/entities/category.entity.ts
+++ src/components/category/entities/category.entity.ts
@@ -10,6 +10,9 @@ export class Category{
     @Column()
     name: string;
 
+    @Column()
+    description: string
+
     @OneToMany(()=>Project,project=>project.category_id)
     project_id: Category[]
 }
