735616240c29edebd3e4f57d3981664e4e500538
thelastgoth128 <thindwacyrus86@gmail.com>
2025-08-09 07:54:38 +0200

    set up project entity

--- src/components/project/entities/project.entity.ts
+++ src/components/project/entities/project.entity.ts
@@ -1,1 +1,26 @@
-export class Project {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity()
+export class Project {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    title: string
+
+    @Column()
+    description: string
+
+    @Column()
+    image_url: string
+
+    @Column()
+    client_name: string
+
+    @Column()
+    category_id: number
+
+    @Column()
+    created_at: Date
+}
