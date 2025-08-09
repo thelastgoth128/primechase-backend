546bbd9f4416f519963934729f87cc857492b1ee
thelastgoth128 <thindwacyrus86@gmail.com>
2025-08-09 09:17:17 +0200

    add validation pipes

--- src/components/project/dto/create-project.dto.ts
+++ src/components/project/dto/create-project.dto.ts
@@ -1,1 +1,45 @@
-export class CreateProjectDto {}
+import { IsDate, IsEmail, IsInt, IsString } from "class-validator"
+
+export class CreateProjectDto {
+        @IsString()
+        title: string
+    
+        @IsString()
+        description: string
+    
+        @IsString()
+        style_reference: string
+    
+        @IsString()
+        color_preferences: string[]
+    
+        @IsString()
+        font_preferences: string
+    
+        @IsString()
+        image_url: string
+    
+        @IsDate()
+        start_date: Date
+    
+        @IsDate()
+        due_date: Date
+    
+        @IsString()
+        client_name: string
+    
+        @IsEmail()
+        client_email: string
+    
+        @IsString()
+        status: string
+    
+        @IsString()
+        rejection_reason: string
+    
+        @IsInt()
+        category_id: number
+    
+        @IsDate()
+        created_at: Date
+}
