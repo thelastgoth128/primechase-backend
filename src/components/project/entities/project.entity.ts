5ed85f7c4b69ac2f5ffbe1b6a893e174fb214454
thelastgoth128 <thindwacyrus86@gmail.com>
2025-08-09 09:09:09 +0200

    add some input columns

--- src/components/project/entities/project.entity.ts
+++ src/components/project/entities/project.entity.ts
@@ -1,4 +1,5 @@
 import { Category } from "src/components/category/entities/category.entity";
+import { Status } from "src/components/enums/status.ennum";
 import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 
 
@@ -14,10 +15,38 @@ export class Project {
     description: string
 
     @Column()
+    style_reference: string
+
+    @Column()
+    color_preferences: string[]
+
+    @Column()
+    font_preferences: string
+
+    @Column()
     image_url: string
 
     @Column()
+    start_date: Date
+
+    @Column()
+    due_date: Date
+
+    @Column()
     client_name: string
+
+    @Column()
+    client_email: string
+
+    @Column({
+        type:'enum',
+        enum:Status,
+        default:Status.pending,
+    })
+    status: string
+
+    @Column()
+    rejection_reason: string
 
     @ManyToOne(()=>Category,category=>category.project_id)
     @JoinColumn({name: 'category_id'})
