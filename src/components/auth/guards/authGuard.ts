0073699a64cd20e6dc55e0debe7ec42108b40caa
thelastgoth128 <thindwacyrus86@gmail.com>
2025-08-11 04:23:12 +0200

    fix class

--- src/components/auth/guards/authGuard.ts
+++ src/components/auth/guards/authGuard.ts
@@ -18,7 +18,7 @@ export class AuthGuard implements CanActivate {
     async canActivate(context: ExecutionContext): Promise<boolean>  {
         const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY,[
             context.getHandler(),
-            context.getHandler()
+            context.getClass()
         ])
 
         if (isPublic) {
