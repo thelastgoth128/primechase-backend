5ac2824b8e326cdbeb746f4dc803066a5fb34748
thelastgoth128 <thindwacyrus86@gmail.com>
2025-08-11 03:57:18 +0200

    import category module in project

--- src/components/project/project.module.ts
+++ src/components/project/project.module.ts
@@ -3,10 +3,11 @@ import { ProjectService } from './project.service';
 import { ProjectController } from './project.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Project } from './entities/project.entity';
+import { CategoryModule } from '../category/category.module';
 
 @Module({
   imports:[
-    TypeOrmModule.forFeature([Project])
+    TypeOrmModule.forFeature([Project]),CategoryModule
   ],
   controllers: [ProjectController],
   providers: [ProjectService],
