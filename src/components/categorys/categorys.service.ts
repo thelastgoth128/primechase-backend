a996a9c344a03c730f08203773630a84ea976aad
thelastgoth128 <thindwacyrus86@gmail.com>
2025-08-13 21:02:12 +0200

    add method to query all category and get one projectg to show case

--- src/components/categorys/categorys.service.ts
+++ src/components/categorys/categorys.service.ts
@@ -4,12 +4,15 @@ import { UpdateCategoryDto } from './dto/update-category.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Category } from './entities/category.entity';
 import { Repository } from 'typeorm';
+import { Project } from '../project/entities/project.entity';
 
 @Injectable()
 export class CategorysService {
   constructor(
     @InjectRepository(Category)
     private readonly catrep : Repository<Category>,
+    @InjectRepository(Project)
+    private readonly projectrep : Repository<Project>,
   ){}
 
   async create(createCategoryDto: CreateCategoryDto) {
@@ -22,6 +25,23 @@ export class CategorysService {
 
   async findOne(id: number) {
     return await this.catrep.findOne({where: {id}});
+  }
+
+  async findCategoriesWithAProject() {
+    const categories = await this.catrep.find()
+
+    const result = await Promise.all(categories.map(async category => {
+      const project = await this.projectrep.findOne({where: { category: {id: category.id}}});
+
+      return {
+        category:{
+          id: category.id,
+          name: category.name,
+        },
+        project: project || null,
+      }
+    }))
+    return result
   }
 
   async update(id: number, updateCategoryDto: UpdateCategoryDto) {
