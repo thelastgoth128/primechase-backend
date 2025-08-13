3a2c3629f2f006ee1ea4538802aaabeb71ea2222
thelastgoth128 <thindwacyrus86@gmail.com>
2025-08-13 11:35:25 +0200

    add a class validator

--- src/components/categorys/dto/create-category.dto.ts
+++ src/components/categorys/dto/create-category.dto.ts
@@ -1,1 +1,9 @@
-export class CreateCategoryDto {}
+import { IsString } from "class-validator";
+
+export class CreateCategoryDto {
+    @IsString()
+    name : string
+
+    @IsString()
+    description : string
+}
