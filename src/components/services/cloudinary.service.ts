b6fe73a1ee7eafc48eebf3d87e9859b2cafa5165
thelastgoth128 <thindwacyrus86@gmail.com>
2025-08-17 12:50:16 +0200

    add transformations to already uploaded images

--- src/components/services/cloudinary.service.ts
+++ src/components/services/cloudinary.service.ts
@@ -1,4 +1,6 @@
 import { UploadApiErrorResponse, UploadApiOptions, UploadApiResponse, v2 } from "cloudinary";
+import { v2 as cloudinary } from 'cloudinary';
+
 
 export class CloudinaryService {
     constructor(){
@@ -7,6 +9,7 @@ export class CloudinaryService {
             api_key: process.env.API_KEY,
             api_secret: process.env.API_SECRET,
         })
+        
     }
     async uploadImage(filePath: string,options?: UploadApiOptions): Promise<UploadApiResponse | UploadApiErrorResponse> {
         return new Promise((resolve, reject) => {
@@ -17,6 +20,17 @@ export class CloudinaryService {
                 if (!result) {
                     return reject(new Error('Upload result is undefined'));
                 }
+                const optimizedUrl = cloudinary.url(result.public_id, {
+                transformation: [
+                    { width: 800, crop: 'limit' },
+                    { quality: 'auto' },
+                    { fetch_format: 'auto' }
+                ],
+                secure:true
+                });
+
+                (result as any).optimized_url = optimizedUrl;
+                
                 resolve(result)
             })
         })
