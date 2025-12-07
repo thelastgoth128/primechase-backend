2629fbce7c74417c42afc4973b903d8852573a59
thelastgoth128 <thindwacyrus86@gmail.com>
2025-12-07 02:56:29 +0200

    feat: add new CategorysModule and ImageModule to the application.

--- src/components/categorys/categorys.module.ts
+++ src/components/categorys/categorys.module.ts
@@ -4,13 +4,14 @@ import { CategorysController } from './categorys.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from './entities/category.entity';
 import { ProjectModule } from '../project/project.module';
+import { Project } from '../project/entities/project.entity';
 
-@Module({   
-  imports:[
-      TypeOrmModule.forFeature([Category]),forwardRef(()=> ProjectModule)
-    ],
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Category, Project]), forwardRef(() => ProjectModule)
+  ],
   controllers: [CategorysController],
   providers: [CategorysService],
-  exports:[TypeOrmModule]
+  exports: [TypeOrmModule, CategorysService]
 })
-export class CategorysModule {}
+export class CategorysModule { }
--- src/components/image/image.module.ts
+++ src/components/image/image.module.ts
@@ -1,11 +1,11 @@
-import { Module } from "@nestjs/common";
+import { forwardRef, Module } from "@nestjs/common";
 import { ProjectModule } from "../project/project.module";
 import { ImageController } from "./image.controller";
 import { ImageService } from "./image.service";
 import { CloudinaryService } from "../services/cloudinary.service";
 
 @Module({
-    imports: [ProjectModule],
+    imports: [forwardRef(() => ProjectModule)],
     controllers: [ImageController],
     providers: [ImageService, CloudinaryService],
     exports: [ImageService, CloudinaryService]
